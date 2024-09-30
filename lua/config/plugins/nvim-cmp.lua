d4169d938a76b1d9e539586e4354790cdb5fb5f1
lfrit <l.fritz694@outlook.com>
2024-09-30 19:45:23 +0200

    added new plugins

--- lua/config/plugins/nvim-cmp.lua
+++ lua/config/plugins/nvim-cmp.lua
@@ -20,7 +20,40 @@ return {
 
         local luasnip = require("luasnip")
         local lspkind = require("lspkind")
+        -- Keymaps for jumping between insert points
+        vim.keymap.set({ "i", "s" }, "<leader>n", function()
+            if luasnip.expand_or_jump then
+                luasnip.expand_or_jump()
+            end
+        end, { silent = false, desc = "Go to next insert point" })
+        vim.keymap.set({ "i", "s" }, "<leader>p", function()
+            if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            end
+        end, { silent = true, desc = "Go to previous insert point" })
 
+        -- Custom snippets
+        luasnip.add_snippets("c", {
+            luasnip.snippet("newFile", {
+                luasnip.text_node({ "/**", " * @file " }),
+                luasnip.function_node(function()
+                    return vim.fn.expand("%:t")
+                end),
+                luasnip.text_node({ "", " * @brief " }),
+                luasnip.insert_node(1, "<short brief>"),
+                luasnip.text_node({ "", " * @author Lukas Fritz (lukas.fritz09@htl-sazburg.ac.at)" }),
+                luasnip.text_node({ "", " * @date " }),
+                luasnip.function_node(function()
+                    return os.date("%Y-%m-%d")
+                end),
+                luasnip.text_node({ "", "cls; gcc " }),
+                luasnip.function_node(function()
+                    return vim.fn.expand("%:t")
+                end),
+                luasnip.text_node(" -Wall -Werror -o run.exe; PAUSE; cls; .\\run.exe"),
+                luasnip.text_node({ "", "*/" }),
+            }),
+        })
         -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
         require("luasnip.loaders.from_vscode").lazy_load()
 
@@ -33,13 +66,26 @@ return {
                     luasnip.lsp_expand(args.body)
                 end,
             },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-k"] = cmp.mapping.select_prev_item(), -- previous suggestion
+            mapping = {
                 ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-                ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+                ["<Down>"] = cmp.mapping.select_next_item(), -- next suggestion
+                ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+                ["<Up>"] = cmp.mapping.select_prev_item(), -- previous suggestion
                 ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-                ["<CR>"] = cmp.mapping.confirm({ select = false }),
-            }),
+                ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+                    if cmp.visible() then
+                        local entry = cmp.get_selected_entry()
+                        if not entry then
+                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                        end
+                        cmp.confirm()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            },
             -- sources for autocompletion
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
