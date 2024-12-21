1f8e428175fbf85c3067869a679e198a012a953a
Lukas Fritz <l.fritz694@oulook.com>
2024-12-21 14:18:37 +0100

    updated windows compatibility

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,8 +1,5 @@
 vim.g.mapleader = " "
 
--- For autosession
-vim.keymap.set("n", "<leader>ww", "<cmd>SessionSave<CR>", { desc = "Write the session" })
-vim.keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore the saved session" })
 -- window management
 vim.keymap.set("n", "<leader>wsv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 vim.keymap.set("n", "<leader>wsh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
@@ -14,14 +11,16 @@ vim.keymap.set("n", "<A-l>", "<cmd>tabnext<CR>", { desc = "Go to next tab" })
 vim.keymap.set("n", "<A-h>", "<cmd>tabprevious<CR>", { desc = "Go to previous tab" })
 vim.keymap.set("n", "<A-k>", "<cmd>bprevious<CR>", { desc = "Go to previous tab" })
 vim.keymap.set("n", "<A-j>", "<cmd>bnext<CR>", { desc = "Go to previous tab" })
-vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines down (Visual)" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected lines up (Visual)" })
 vim.keymap.set("t", "<esc>", '<C-\\><C-n>"', { desc = "Exit Terminal" })
 vim.keymap.set("n", "<leader>wt", function()
-  vim.fn.termopen(vim.o.shell)
-  vim.cmd.terminal()
+  if vim.fn.has("win32") == 1 then
+    vim.cmd.terminal("powershell")
+  else
+    vim.cmd.terminal()
+  end
   local win_id = vim.api.nvim_get_current_win()
   vim.api.nvim_win_set_width(win_id, math.max(80, vim.api.nvim_win_get_height(win_id)))
 end, { desc = "Open terminal in split" })
@@ -64,7 +63,11 @@ local function toggle_floating_terminal()
     floating_terminal.buf = vim.api.nvim_create_buf(false, true)
     floating_terminal.win = open_floating_window(floating_terminal.buf)
 
-    vim.fn.termopen(vim.o.shell)
+    if vim.fn.has("win32") == 1 then
+      vim.cmd.terminal("powershell")
+    else
+      vim.cmd.terminal()
+    end
     vim.api.nvim_buf_set_option(floating_terminal.buf, "buflisted", false)
   end
 end
