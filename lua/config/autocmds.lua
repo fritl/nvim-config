3c7ab1b2b87cee4c32ebcefd56419cf34092dfc5
Lukas Fritz <l.fritz694@oulook.com>
2024-12-21 14:10:01 +0100

    remove remove command before compiling

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,7 +6,6 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   callback = function()
     local file = vim.fn.expand("%")
     local output_file = vim.loop.os_uname().sysname:match("Windows") and "run.exe" or "run.out"
-    vim.fn.jobstart({ "rm", output_file })
     vim.fn.jobstart({ "gcc", file, "-o", output_file }, {
       on_stderr = function(_, data, _)
         if data then
