cc446bf99ff2ed7050cb2abc72c5ba8544751f88
Lukas Fritz <l.fritz694@gmail.com>
2025-08-05 22:02:56 +0200

    yay

--- lua/plugins/jupynium.lua
+++ lua/plugins/jupynium.lua
@@ -5,14 +5,15 @@ return {
         "kiyoon/jupynium.nvim",
 
         build = (vim.loop.os_uname().sysname == "Windows_NT") and
-            "pip install --user ." or
+            "python3 -m pip install --user ." or
             "python3 -m venv venv && source venv/bin/activate && pip3 install .",
         -- build = "uv pip install . --python=$HOME/.virtualenvs/jupynium/bin/python",
         -- build = "conda run --no-capture-output -n jupynium pip install .",
         config = function()
             local plugin_path = vim.fn.stdpath("data") .. "/lazy/jupynium.nvim/"
             require("jupynium").setup({
-                python_host = plugin_path .. "venv/bin/python",
+                python_host = (vim.loop.os_uname().sysname == "Windows_NT") and "python3" or
+                    plugin_path .. "venv/bin/python",
                 default_notebook_URL = "localhost:8888/",
             })
         end
